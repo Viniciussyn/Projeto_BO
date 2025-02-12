4b505cd205c0b846eedbaf7f4a4e50cfb537f45c
Vinicius Soares <viniciussyn2000@gmail.com>
2025-02-11 21:26:06 -0300

    Projeto integrador em sí

---
+++ ProjetoIntegrador_OB/ProjetoIntegrador_OB/Pessoas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProjetoIntegrador_OB
+{
+    public class Pessoas
+    {
+        public int idade { get; private set; }
+        public string nome { get; private set; }
+
+        /*-----------------------------------------*/
+
+        public Pessoas(int Idade , string Nome)
+        {
+            nome = Nome;
+            idade = Idade;
+        }
+
+
+    }
+}
---
+++ ProjetoIntegrador_OB/ProjetoIntegrador_OB/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace ProjetoIntegrador_OB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoas x = new Pessoas(26 , "Vinicius Silva Soares");
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
